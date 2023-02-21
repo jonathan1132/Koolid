91babcac689cd7065346f8addb87985be3bdb15f
opilane <opilane@HP-CZC0359NNV>
2023-02-21 09:22:54 +0200

    Add project files.

---
+++ Koolid/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koolid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> koolid = new Dictionary<int, string>();
+            koolid.Add(1, "Tallinna Rahumäe Põhikool");
+            koolid.Add(2, "Tallinna Nõmme Gümnaasium");
+            koolid.Add(3, "Tallinna 32. keskkool");
+            
+            foreach (KeyValuePair<int, string> kool in koolid)
+            {
+                Console.WriteLine("Kooli nimi: {1}", kool.Key, kool.Value);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
